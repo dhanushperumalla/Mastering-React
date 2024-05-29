1b19a566b7c6f43096026477516c172ba4a1d25c
Perumalla Venkata Naga Dhansush <perumalladhanush102@gmail.com>
2024-05-29 21:13:26 +0530

    Password Genrator

--- Project-4/src/App.jsx
+++ Project-4/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
-  //useRef hook
   const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
